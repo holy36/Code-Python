3ee7b599e5a1dfeb5278944b9d9fc41ea36e8612
Đoàn Quang Lưu <115910516+holy36@users.noreply.github.com>
2023-02-23 11:24:16 +0700

    Add files via upload

---
+++ reversed string.py
@@ -0,0 +1,10 @@
+
+# Đảo ngược chuỗi sử dụng slicing
+
+my_string = "ABCDE"
+reversed_string = my_string[::-1]
+
+print(reversed_string)
+
+# Kết quả
+# EDCBA
---
+++ viet hoa ky tu dau.py
@@ -0,0 +1,9 @@
+my_string = "đây là một chuỗi"
+
+# Sử dụng hàm title() của lớp string
+new_string = my_string.title()
+
+print(new_string)
+
+# kết quả
+# Đây Là Một Chuỗi
